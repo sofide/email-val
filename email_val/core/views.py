bdafc5b93a9423f6e7993aecbab44a9e0d54d3fc
Sofia Denner <sofi.denner@gmail.com>
2018-02-06 20:51:21 -0300

    form added to view

--- email_val/core/views.py
+++ email_val/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from core.forms import UploadCsvForm
+
 
 def home(request):
-    return render(request, 'core/home.html')
+    if request.method == 'POST':
+        csv_form = UploadCsvForm(request.POST)
+
+        if csv_form.is_valid():
+            return render(request, 'core/result.html')
+
+    else:
+        csv_form = UploadCsvForm()
+
+    return render(request, 'core/home.html', {'csv_form': csv_form})
