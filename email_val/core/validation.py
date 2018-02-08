d355f4ad6e2dac10fc3e1ecd5291441c87dceb95
Sofia Denner <sofi.denner@gmail.com>
2018-02-08 01:17:26 -0300

    random validation and validation list

--- email_val/core/validation.py
+++ email_val/core/validation.py
@@ -1,12 +1,15 @@
 import urllib
 import json
+import random
+
+from collections import defaultdict
 
 
 API_URL = 'https://bpi.briteverify.com/emails.json?'
 API_KEY = None
 
 
-def validate(email):
+def api_validation(email):
     url = API_URL + urllib.parse.urlencode({'adress': email, 'apikey': API_KEY})
     data = urllib.request.urlopen(url)
     data = data.read().decode()
@@ -14,3 +17,29 @@ def validate(email):
     result = json.loads(data)
 
     return result
+
+
+def random_validation(email):
+    result = {
+        'address': email,
+        'status': random.choice(('valid', 'invalid', 'unknown', 'accept all'))
+    }
+
+    return result
+
+
+def validator(emails_list):
+    if API_KEY:
+        validation_function = api_validation
+        validation_from = 'api'
+    else:
+        validation_function = random_validation
+        validation_from = 'random'
+
+    validations = defaultdict(list)
+
+    for email in emails_list:
+        result = validation_function
+        validations[result['status']].append(email)
+
+    return validations, validation_from
