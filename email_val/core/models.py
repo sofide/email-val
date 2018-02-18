601bf6680d72cc1d61bd252f26b08504bfe157d8
Sofia Denner <sofi.denner@gmail.com>
2018-02-18 20:24:04 -0300

    email model

--- email_val/core/models.py
+++ email_val/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class email(models.Model):
+    VALID = 'va'
+    INVALID = 'in'
+    UNKNOWN = 'un'
+    ACCEPT_all = 'aa'
+
+    STATUS_CHOICES = (
+        (VALID, 'Valid'),
+        (INVALID, 'Invalid'),
+        (UNKNOWN, 'Unknown'),
+        (ACCEPT_all, 'Accept all'),
+    )
+
+    email = models.CharField(max_length=254, unique=True)
+    last_validation = models.DateField(auto_now=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    error = models.CharField(max_length=200, blank=True)
+    is_role_address = models.BooleanField(default=False)
