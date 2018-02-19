d7cc0b608ee174e9ac1dc4d59b7d6b941c3d18f1
Sofia Denner <sofi.denner@gmail.com>
2018-02-19 01:16:11 -0300

    pytest

--- email_val/core/tests.py
+++ email_val/core/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from core.validation import save_email, random_validation
+from core.models import Email, Status
+
+
+@pytest.mark.django_db
+def test_save_email():
+    email = 'prueba@bla.com'
+    validation = random_validation(email)
+    save_email(validation)
+    assert Email.objects.filter(email=email).exists()
