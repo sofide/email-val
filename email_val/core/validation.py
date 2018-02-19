65067b543ce27d4ef4e8541d845be7b29f5cb2bf
Sofia Denner <sofi.denner@gmail.com>
2018-02-18 21:33:47 -0300

    save email to db function

--- email_val/core/validation.py
+++ email_val/core/validation.py
@@ -3,6 +3,8 @@ import requests
 
 from collections import defaultdict
 
+from core.models import Email, Status
+
 
 API_URL = 'https://bpi.briteverify.com/emails.json'
 API_KEY = None
@@ -24,6 +26,27 @@ def random_validation(email):
     }
 
     return result
+
+
+def save_email(api_validation):
+    address = api_validation['address']
+    status_label = api_validation['status']
+
+    if not Status.objects.filter(status=status_label).exists():
+        Status.objects.create(status=status_label)
+    status = Status.objects.filter(status=status_label).first()
+
+    if Email.objects.filter(email=address).exists():
+        email = Email.objects.filter(email=address).first()
+    else:
+        email = Email(email=address)
+
+    email.status = status
+    email.is_role_address = api_validation['role_address']
+    if 'error' in api_validation:
+        email.error = api_validation['error']
+
+    email.save()
 
 
 def validator(emails_list):
